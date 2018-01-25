3b409b71d21c6bc3447324b3d602d689efbc5471
Romulo S. de Souza <JG40@MI00314326.petrobras.biz>
2018-01-25 14:09:20 -0200

    Adiconando Exercicios do Curso

--- Alura/src/alura/Alura.java
+++ Alura/src/alura/Alura.java
@@ -15,7 +15,7 @@ public class Alura {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bla BLA");
+        System.out.println("Bla BLA o");
     }
     
 }
